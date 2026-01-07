dbf1b55b67534e4b33cd923b5136f667d6247b9f
Skyler Fokine <skylerfokine@gmail.com>
2026-01-06 22:48:44 -0500

    got bootstrap to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { HeadshotCard } from "./componets/HeadshotCard";
 
-function App() {
-  
+export default function App() {
   return (
-    
-    
-  )
+    <div className="d-flex align-items-start">
+      <div>
+        <HeadshotCard />
+      </div>
+      <h1> hello world </h1>
+    </div>
+  );
 }
-
-export default App
--- src/componets/HeadshotCard.jsx
+++ src/componets/HeadshotCard.jsx
@@ -1,5 +1,7 @@
-import Card from "react-bootstrap/card";
-import Button from "react-bootstrap/button";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
+import headshot from "../assets/headshot.jpg";
 
 const aboutMe = {
   name: "Skyler Fokine",
@@ -7,21 +9,29 @@ const aboutMe = {
   blurb: "Computer Science and Artifical Intelligence Major",
 };
 
-export default function HeadshotCard() {
+export function HeadshotCard() {
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Image src="holder.js/171x180" roundedCircle />
+      <Card style={{ width: "344px", height: "640px" }}>
         <Card.Body>
+          <div
+            className="w-100 overflow-hidden rounded-4"
+            style={{ height: "180px" }}
+          >
+            <Image src={headshot} className="w-100 h-100 object-fit-cover " />
+          </div>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-        </Card.Body>
-        <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
+          <div className="d-flex align-items-start">
+            <Nav.Item>
+              <Nav.Link eventKey="1" href="#/home">
+                NavLink 1 content
+              </Nav.Link>
+            </Nav.Item>
+          </div>
         </Card.Body>
       </Card>
     </>
